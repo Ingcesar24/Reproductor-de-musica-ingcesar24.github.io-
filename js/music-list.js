dc3c195742057daf2f05dcdeb523f4bc26757b66
Ingcesar24 <99461334+Ingcesar24@users.noreply.github.com>
2024-03-16 23:04:19 -0500

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,52 @@
+let allMusic = [
+  {
+    name: "Pero Te Conocí",
+    artist: "Reik ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Usted Es Hermosa",
+    artist: "Fausto Miño",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Entra en mi vida",
+    artist: "Sin Bandera",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Aprender A Quererte",
+    artist: "Morat",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Me entrego",
+    artist: "KURT - ft Juan Solo",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lo Quiero Todo",
+    artist: "Macaco",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "La Mujer Perfecta",
+    artist: "Kurt",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Virgen",
+    artist: "Adolescent's Orquesta",
+    img: "music-8",
+    src: "music-8"
+  },
+
+
+];
